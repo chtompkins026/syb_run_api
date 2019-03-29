ad27021bf38ba94880236d557e138b019d4163ba
chtompkins026 <chtompkins026@gmail.com>
2019-03-29 13:07:56 -0400

    add in workout payment associations

--- app/models/workout_payment.rb
+++ app/models/workout_payment.rb
@@ -1,2 +1,7 @@
 class WorkoutPayment < ApplicationRecord
+  belongs_to :account, :inverse_of => :workout_payments
+  accepts_nested_attributes_for :account
+
+  belongs_to :booking, :inverse_of => :workout_payments
+  accepts_nested_attributes_for :booking
 end
