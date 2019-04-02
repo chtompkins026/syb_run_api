03ae6f18fc1ddf47e2d1c5d9d4c937862173cce8
chtompkins026 <chtompkins026@gmail.com>
2019-04-02 11:15:43 -0400

    Add in devise logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,29 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  namespace :api do
-    resources :clients
-    resources :trainers
-    resources :users
-    resources :lessons
+  scope module: :api, defaults: { format: :json }, path: 'api' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      devise_for :users, controllers: {
+           registrations: 'api/v1/users/registrations',
+       }, skip: [:sessions, :password]
 
-    devise_for :users,
-             path: '',
-             path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
-             },
-             controllers: {
-               sessions: 'sessions',
-               registrations: 'registrations'
-             }
-  end #end of namespace for API 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+       resources :clients
+       resources :trainers
+       resources :users
+       resources :lessons
+    end
+  end
 end
+
+
+# devise_for :users,
+#          path: '',
+#          path_names: {
+#            sign_in: 'login',
+#            sign_out: 'logout',
+#            registration: 'signup'
+#          },
+#          controllers: {
+#            sessions: 'sessions',
+#            registrations: 'registrations'
+#          }
