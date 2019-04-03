a5d9f81f5c7d76a6b30fa7f73ae0cc55fb7fb790
chtompkins026 <chtompkins026@gmail.com>
2019-04-03 14:32:09 -0400

    Took out inverse association on the client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,7 +6,7 @@ class Client < ApplicationRecord
   belongs_to :user#, :inverse_of => :clients
   accepts_nested_attributes_for :user
 
-  has_many :bookings, dependent: :destroy, :inverse_of => :client
+  has_many :bookings, dependent: :destroy #, :inverse_of => :client
   accepts_nested_attributes_for :bookings
 
   has_many :workouts, :through => :bookings
