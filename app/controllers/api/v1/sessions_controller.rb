61dea7b96507de0cd75e98c5b7441936d84a6766
chtompkins026 <chtompkins026@gmail.com>
2019-04-04 10:38:03 -0400

    Working on modifying the registrations controller to return token upon sing up. Currently added in more devise methods and will test out

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,6 @@
-class Api::SessionsController < Devise::SessionsController
+class Api::V1::SessionsController < Devise::SessionsController
+
   respond_to :json
-
-  def new
-
-  end
 
   def create
    user = User.find_by(email: params[:session][:email].downcase)
