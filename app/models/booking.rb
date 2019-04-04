3d19a9dc5f97bf60404710a848fadae850be9c14
chtompkins026 <chtompkins026@gmail.com>
2019-04-04 11:58:15 -0400

    Fixed booking model association

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   # belongs_to :account #:inverse_of => :bookings
   # accepts_nested_attributes_for :account
-  
+
   belongs_to :user #:inverse_of => :bookings
   accepts_nested_attributes_for :user
 
@@ -11,8 +11,8 @@ class Booking < ApplicationRecord
   belongs_to :schedule #, :inverse_of => :bookings
   accepts_nested_attributes_for :schedule
 
-  belongs_to :trainer #:inverse_of => :bookings
-  accepts_nested_attributes_for :trainer
+  belongs_to :instructor #:inverse_of => :bookings
+  accepts_nested_attributes_for :instructor
 
   belongs_to :client # :inverse_of => :bookings
   accepts_nested_attributes_for :client
