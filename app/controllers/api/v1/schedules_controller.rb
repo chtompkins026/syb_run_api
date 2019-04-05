1f8fa957ad76f9927bf35ba24c9e32d54c6ecdc7
chtompkins026 <chtompkins026@gmail.com>
2019-04-05 15:41:49 -0400

    Fixed methods on schedules controller

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -1,70 +1,62 @@
 class Api::V1::SchedulesController < ApplicationController
+  before_action :set_schedule, only: [:show, :edit, :update, :destroy]
 
   def index
-      @schedules = Schedule.all
-      render json: @schedules
+    @schedules = Schedule.all
+    render json: @schedules
   end
 
   # GET /schedules/1.json
   def show
-    @schedule = Schedule.find_by(id: params[:id])
     render json: @schedule
   end
 
-    # GET /schedules/new
-    def new
-      @schedule = Schedule.new
-      render json: @schedule
-    end
+  # GET /schedules/new
+  def new
+    @schedule = Schedule.new
+    render json: @schedule
+  end
 
-    # GET /schedules/1/edit
-    def edit
-    end
+  # GET /schedules/1/edit
+  def edit
+  end
 
-    # POST /schedules
-    # POST /schedules.json
-    def create
-      @schedule = Schedule.new(schedule_params)
-
-      respond_to do |format|
-        if @schedule.save
-          format.json { render json: @schedule, status: :created, location: @schedule }
-        else
-          format.json { render json: @schedule.errors, status: :unprocessable_entity }
-        end
+  # POST /schedules
+  # POST /schedules.json
+  def create
+    @schedule = Schedule.new(schedule_params)
+      if @schedule.save
+        render json: @schedule
+      else
+        render json: @schedule.errors
       end
-    end
+  end
 
-    # PATCH/PUT /schedules/1
-    # PATCH/PUT /schedules/1.json
-    def update
-      respond_to do |format|
-        if @schedule.update(schedule_params)
-          format.json { render json: @schedule, status: :ok, location: @schedule }
-        else
-          format.json { render json: @schedule.errors, status: :unprocessable_entity }
-        end
+  # PATCH/PUT /schedules/1
+  # PATCH/PUT /schedules/1.json
+  def update
+      if @schedule.update(schedule_params)
+        render json: @schedule
+      else
+        render json: @schedule.errors
       end
-    end
+  end
 
-    # DELETE /schedule/1
-    # DELETE /schedule/1.json
-    def destroy
-      @schedule.destroy
-      respond_to do |format|
-        format.json { head :no_content }
-      end
-    end
+  # DELETE /schedule/1
+  # DELETE /schedule/1.json
+  def destroy
+    @schedule.destroy
+  end
 
 private
   # Never trust parameters from the scary internet, only allow the white list through.
   def schedule_params
-    params.require(:schedule).permit(:title, :start, :end, :instructor_id,
-      :created_at, :updated_at, :workout_id)
+    params.permit(:title, :start, :end, :instructor_id,
+      :workout_id)
   end
 
-  def set_workout
-    @workout = Workout.find(params[:workout_id])
+  def set_schedule
+    @schedule = Schedule.find_by(id: params[:id])
   end
 
 end
