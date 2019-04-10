3c99b856a11d167236e874448114598efd50a7c9
chtompkins026 <chtompkins026@gmail.com>
2019-04-10 10:46:30 -0400

    Fix client serializer, Random comma

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,4 +1,4 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes id:,:first_name, :last_name, :phone, :instagram, :user_id
+  attributes id:, :first_name, :last_name, :phone, :instagram, :user_id
   has_many :bookings
 end
