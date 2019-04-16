434789958168071e2f1119be68b2c1a40d1b6f37
chtompkins026 <chtompkins026@gmail.com>
2019-04-16 10:59:09 -0400

    Added client data to response back when user logs in via doorkeeper

--- lib/custom_token_response.rb
+++ lib/custom_token_response.rb
@@ -1,7 +1,7 @@
 module CustomTokenResponse
   def body
     user = User.find(@token.resource_owner_id)
-    client = user.client
+    client = user.client  
     # call original `#body` method and merge its result with the additional data hash
        super.merge({
            status_code: 200,
